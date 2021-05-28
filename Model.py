d57864b488282e8882f11bb5d4bad61f71412af5
carterrxavier <carter.r.xavier@gmail.com>
2021-05-27 22:35:38 -0500

    added function for targeting high risk customers

--- Model.py
+++ Model.py
@@ -63,7 +63,29 @@ def test_results(model, X_test, y_test, details=False):
         print(pd.DataFrame(classification_report(y_test, t_pred, output_dict =True)))
         print('-----Train Confusion Matrix------')
         print(pd.DataFrame(confusion_matrix(t_pred, y_test), index=Row_labels, columns=Col_labels))
+        
+def get_customer_predictions(model, dataframe , X_var, churn_status = 0, high_risk_percentage = 0):
     
+    '''
+    returns highrisk customer dataframe based on there churn status and if their probability of churning is higher than the high risk percentage specified
+    '''
+    #establish high risk as percentage
+    high_risk_percentage = high_risk_percentage * 100
+    
+    y_pred = model.predict(X_var)
+    pred_churn_df = dataframe[['customer_id','churn']]
+    proba = model.predict_proba(X_var) 
+    proba_list= proba.tolist()
+    churn_prob =[]
+    for item in proba_list:
+        churn_prob.append(item[1] * 100)
+    df_prob = X_var.copy()
+    pred_churn_df['churn prediction'] =  y_pred
+    pred_churn_df['churn probability'] = churn_prob
+    
+    
+    
+    return pred_churn_df[(pred_churn_df['churn'] == churn_status) & (pred_churn_df['churn probability'] >= high_risk_percentage)].sort_values(by='churn probability', ascending=False)
 
 
 
